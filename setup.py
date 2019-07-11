dc4c6b72cf8ab1e4f20f8aec85b31dc58f2f6f27
nilsec <nils.eckstein@googlemail.com>
2019-07-11 17:18:40 -0400

    Add minimal setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='synister',
+    version='0.1',
+    description='Neurotransmitter classification',
+    url='https://github.com/funkelab/synister',
+    author='Funkelab',
+    packages=[
+        'synister'
+        ])
