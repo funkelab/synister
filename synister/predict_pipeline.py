b6249ec1bde055df8eef90f3780c89204f64ab2f
nilsec <nils.eckstein@googlemail.com>
2019-10-07 19:21:29 -0400

    Add parallel predict

--- synister/predict_pipeline.py
+++ synister/predict_pipeline.py
@@ -4,14 +4,17 @@ import numpy as np
 
 from synister.utils import init_vgg, predict, get_raw
 from synister.synister_db import SynisterDB
-from synister.read_config import read_predict_config
+from synister.read_config import read_predict_config, read_worker_config
 
 import logging
+import multiprocessing
+import sys
 
 logger = logging.getLogger(__name__)
 
 
-def test(train_checkpoint,
+def test(worker_id,
+         train_checkpoint,
          db_credentials,
          db_name_data,
          split_name,
@@ -25,7 +28,10 @@ def test(train_checkpoint,
          raw_dataset,
          experiment,
          train_number,
-         predict_number):
+         predict_number,
+         num_cache_workers,
+         num_block_workers,
+         **kwargs):
 
 
     model = init_vgg(train_checkpoint,
@@ -37,44 +43,79 @@ def test(train_checkpoint,
 
     logger.info('Load test sample locations from db {} and split {}...'.format(db_name_data, split_name))
     db = SynisterDB(db_credentials)
-    
-    db.initialize_prediction(db_name_data,
-                             split_name,
-                             experiment,
-                             train_number,
-                             predict_number)
+
+    logger.info('Initialize prediction writers...')
+    prediction_queue = multiprocessing.JoinableQueue()
+
+    for i in range(num_cache_workers):
+        worker = multiprocessing.Process(target=prediction_writer,
+                                         args=(prediction_queue,
+                                               db_credentials,
+                                               db_name_data,
+                                               split_name,
+                                               experiment,
+                                               train_number,
+                                               predict_number))
+        #worker.daemon = True
+        worker.start()
+
 
     logger.info('Start prediction...')
-    predictions = []
+
+    locations = []
     for synapse_type in synapse_types:
         logger.info('Predict synapse type {}...'.format(synapse_type))
-        locations = db.get_synapse_locations(db_name_data,
-                                             split_name,
-                                             "test",
-                                             tuple([synapse_type]))
+        locations.extend(db.get_synapse_locations(db_name_data,
+                                                  split_name,
+                                                  "test",
+                                                  tuple([synapse_type])))
+
+
+    loc_start = int(float(worker_id)/num_block_workers) * len(locations) 
+    loc_end = int(float(worker_id + 1)/num_block_workers) * len(locations)
+    my_locations = locations[loc_start:loc_end]
  
+    for i in range(0, len(my_locations), batch_size):
+        logger.info('Predict location {}/{} ({})'.format(i, len(locations), synapse_type))
+        locs = my_locations[i:i+batch_size]
+        raw, raw_normalized = get_raw(locs,
+                                      input_shape,
+                                      voxel_size,
+                                      raw_container,
+                                      raw_dataset)
+        output = predict(raw_normalized, model)
 
-        for i in range(0, len(locations), batch_size):
-            logger.info('Predict location {}/{} ({})'.format(i, len(locations), synapse_type))
-            locs = locations[i:i+batch_size]
-            raw, raw_normalized = get_raw(locs,
-                                          input_shape,
-                                          voxel_size,
-                                          raw_container,
-                                          raw_dataset)
-            output = predict(raw_normalized, model)
+        for k in range(np.shape(output)[0]):
+            loc_k = locs[k]
+            out_k = output[k,:]
+            loc_k_list = loc_k
 
-            for k in range(np.shape(output)[0]):
-                loc_k = locs[k]
-                out_k = output[k,:]
-                loc_k_list = loc_k
+            data_synapse = {"prediction": out_k.tolist(),
+                            "z": loc_k_list[0],
+                            "y": loc_k_list[1],
+                            "x": loc_k_list[2]}
 
-                data_synapse = {"prediction": out_k.tolist(),
-                                "z": loc_k_list[0],
-                                "y": loc_k_list[1],
-                                "x": loc_k_list[2]}
+            prediction_queue.put(data_synapse)
 
-                db.write_prediction(db_name_data,
+    logger.info("Wait for write...")
+    prediction_queue.join()
+
+
+def prediction_writer(prediction_queue,
+                      db_credentials,
+                      db_name_data,
+                      split_name,
+                      experiment,
+                      train_number,
+                      predict_number):
+
+
+    db = SynisterDB(db_credentials)
+    
+    while True:
+        data_synapse = prediction_queue.get()
+
+        db.write_prediction(db_name_data,
                             split_name,
                             data_synapse["prediction"],
                             experiment,
@@ -84,10 +125,16 @@ def test(train_checkpoint,
                             data_synapse["y"],
                             data_synapse["z"])
 
-                predictions.append(data_synapse)
+        prediction_queue.task_done()
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
+    worker_id = int(sys.argv[1])
+    num_block_workers = int(sys.argv[2])
+
     predict_config = read_predict_config("./predict_config.ini")
-    test(**predict_config)
+    worker_config = read_worker_config("./worker_config.ini")
+    worker_config["worker_id"] = worker_id
+    worker_config["num_block_workers"] = num_block_workers
+    test(**{**predict_config, **worker_config})
