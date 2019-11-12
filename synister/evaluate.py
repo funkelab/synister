c096d455c9034ccabfd5045b760eb740fe31e031
nilsec <nils.eckstein@googlemail.com>
2019-11-12 08:06:38 -0500

    Refactor

--- synister/evaluate.py
+++ synister/evaluate.py
@@ -18,6 +18,12 @@ def parse_prediction(db_credentials,
                                      predict_cfg["train_number"],
                                      predict_cfg["predict_number"])
 
+    # Read split always from refactor,
+    # This ensures backwards compatibility 
+    # with v2:
+    if predict_cfg["train_number"] < 19:
+        db = SynisterDb(db_credentials, "synister_v2_refactor")
+
     synapses = db.get_synapses()
     skeletons = db.get_skeletons()
 
@@ -33,6 +39,62 @@ def parse_prediction(db_credentials,
     }
 
     return predicted_synapses, predict_cfg
+
+def synaptic_cross_confusion_matrix(synapses_target, synapses_source, 
+                                    predict_config, normalize=False):
+
+    synapse_types = predict_config["synapse_types"]
+    cm = np.zeros([len(synapse_types)] * 2, dtype=float)
+
+    n = 0
+    for synapse_id, synapse_data in synapses_source.items():
+        if synapse_data["prediction"] == "null":
+            continue
+
+        source_class = np.argmax(synapse_data["prediction"])
+        target_class = np.argmax(synapses_target[synapse_id]["prediction"])
+
+        cm[target_class, source_class] += 1
+        n += 1
+
+    if normalize:
+        cm_row_sum = np.sum(cm, axis=1)
+        cm = (cm.transpose()/cm_row_sum).transpose()
+
+    return cm
+
+def skeleton_cross_confusion_matrix(synapses_target, synapses_source, predict_config, normalize=False):
+    synapse_types = predict_config["synapse_types"]
+    cm = np.zeros([len(synapse_types)] * 2, dtype=float)
+
+    skeleton_ids = set([s["skeleton_id"] for s in synapses_source.values()])
+    skeleton_to_source_prediction = {skid: [] for skid in skeleton_ids}
+    skeleton_to_target_prediction = {skid: [] for skid in skeleton_ids}
+
+    for synapse_id, synapse_data in synapses_source.items():
+        zeros_source = np.zeros(len(synapse_types))
+        arg_max_source = np.argmax(synapse_data["prediction"])
+        zeros_source[arg_max_source] = 1
+        skeleton_to_source_prediction[synapse_data["skeleton_id"]].append(
+                zeros_source)
+
+        zeros_target = np.zeros(len(synapse_types))
+        arg_max_target = np.argmax(synapses_target[synapse_id]["prediction"])
+        zeros_target[arg_max_target] = 1
+        skeleton_to_target_prediction[synapse_data["skeleton_id"]].append(
+                zeros_target)
+
+    for skeleton_id, predictions in skeleton_to_source_prediction.items():
+        majority_vote_source = np.argmax(np.sum(np.array(predictions), axis=0))
+        majority_vote_target = np.argmax(np.sum(np.array(skeleton_to_target_prediction[skeleton_id]), axis=0))
+ 
+        cm[majority_vote_target, majority_vote_source] += 1
+
+    if normalize:
+        cm_row_sum = np.sum(cm, axis=1)
+        cm = (cm.transpose()/cm_row_sum).transpose()
+
+    return cm
 
 def expected_probability_matrix(synapses, predict_config):
     synapse_types = predict_config["synapse_types"]
@@ -63,9 +125,9 @@ def expected_probability_matrix(synapses, predict_config):
 
     return confusion_matrix
 
-def confusion_matrix(synapses, predict_config, normalization_factor=None):
+def synaptic_confusion_matrix(synapses, predict_config, normalization_factor=None, normalize=False):
     synapse_types = predict_config["synapse_types"]
-    confusion_matrix = np.zeros([len(synapse_types)] * 2, dtype=float)
+    cm = np.zeros([len(synapse_types)] * 2, dtype=float)
 
     if normalization_factor is None:
         normalization_factor = {synapse_id: 1. for synapse_id in synapses}
@@ -73,9 +135,7 @@ def confusion_matrix(synapses, predict_config, normalization_factor=None):
     n = 0
     for synapse_id, synapse_data in synapses.items():
         if synapse_data["prediction"] == "null":
-            print("skip")
             continue
-        print("Insert synapse {}/{}".format(n + 1, len(synapses)))
         nt_known = synapse_data["nt_known"]
         if len(nt_known)>1:
             raise Warning("More than one known nt")
@@ -84,14 +144,18 @@ def confusion_matrix(synapses, predict_config, normalization_factor=None):
         gt_class = synapse_types.index(nt_known)
         predicted_class = np.argmax(synapse_data["prediction"])
 
-        confusion_matrix[gt_class, predicted_class] += 1 * normalization_factor[synapse_id]
+        cm[gt_class, predicted_class] += 1 * normalization_factor[synapse_id]
         n += 1
 
-    return confusion_matrix
+    if normalize:
+        cm_row_sum = np.sum(cm, axis=1)
+        cm = (cm.transpose()/cm_row_sum).transpose()
 
-def skeleton_confusion_matrix(synapses, predict_config):
+    return cm
+
+def skeleton_confusion_matrix(synapses, predict_config, normalize=False):
     synapse_types = predict_config["synapse_types"]
-    confusion_matrix = np.zeros([len(synapse_types)] * 2, dtype=float)
+    cm = np.zeros([len(synapse_types)] * 2, dtype=float)
     skeleton_ids = set([s["skeleton_id"] for s in synapses.values()])
     skeleton_to_prediction = {skid: [] for skid in skeleton_ids}
     skeleton_to_gt = {}
@@ -107,50 +171,47 @@ def skeleton_confusion_matrix(synapses, predict_config):
 
     for skeleton_id, predictions in skeleton_to_prediction.items():
         majority_vote = np.argmax(np.sum(np.array(predictions), axis=0))
-        confusion_matrix[skeleton_to_gt[skeleton_id], majority_vote] += 1
+        cm[skeleton_to_gt[skeleton_id], majority_vote] += 1
 
-    return confusion_matrix
+    if normalize:
+        cm_row_sum = np.sum(cm, axis=1)
+        cm = (cm.transpose()/cm_row_sum).transpose()
 
-def plot_confusion_matrix(cm, predict_cfg, name=""):
+    return cm
+
+def plot_confusion_matrix(heatmap_values, predict_cfg, 
+                          annotation_values=None, name="",
+                          save=False,
+                          xlabel="Predicted",
+                          ylabel="Actual"):
     synapse_types = predict_cfg["synapse_types"]
-    df_cm = pd.DataFrame(cm, index = [i for i in synapse_types],
-                                columns = [i for i in synapse_types])
+    df_hm = pd.DataFrame(heatmap_values, index = [i for i in synapse_types],
+                                         columns = [i for i in synapse_types])
+    if annotation_values is not None:
+        annotations = pd.DataFrame(annotation_values, index = [i for i in synapse_types],
+                                                      columns = [i for i in synapse_types])
+    else:
+        annotations = True
+ 
     plt.figure(figsize = (10,10))
-    ax = sn.heatmap(df_cm, annot=True)
+    ax = sn.heatmap(df_hm, annot=annotations, fmt="")
+
     bottom, top = ax.get_ylim()
     ax.set_ylim(bottom+.5, top-.5)
-    plt.ylabel("Actual")
-    plt.xlabel("Predicted")
-    plt.show()
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
     train_number = predict_cfg["train_number"]
     iteration = int(predict_cfg["train_checkpoint"].split("_")[-1])
     plt.title("Confusion Matrix t{} i{} {}".format(train_number, iteration, name))
-    if name:
-        name = "_" + name
-    plt.savefig("confusion_matrix_t{}_i{}{}".format(train_number, iteration, name))
-
-def plot_confusion_matrix_normalized(cm, predict_cfg, name=""):
-    synapse_types = predict_cfg["synapse_types"]
-    cm_row_sum = np.sum(cm, axis=1)
-    cm_normalized = (cm.transpose()/cm_row_sum).transpose()
-    df_cm = pd.DataFrame(cm_normalized, index = [i for i in synapse_types],
-                                columns = [i for i in synapse_types])
-    plt.figure(figsize = (10,10))
-    ax = sn.heatmap(df_cm, annot=True)
-    bottom, top = ax.get_ylim()
-    ax.set_ylim(bottom+.5, top-.5)
-    plt.ylabel("Actual")
-    plt.xlabel("Predicted")
-    checkpoint = predict_cfg["train_checkpoint"]
-    iteration = checkpoint[checkpoint.rindex("_")+1:]
-    train_number = predict_cfg["train_number"]
-    plt.title("Normalized Confusion Matrix t{} i{} {}".format(train_number, iteration, name))
-    if name:
-        name = "_" + name
-    plt.savefig("normalized_confusion_matrix_t{}_i{}{}".format(train_number, iteration, name))
     plt.show()
 
-def find_accuracy(confusion_matrix):                         #returns a tuple (overall accuracy, average accuracy)
+    if save:
+        if name:
+            name = "_" + name
+        plt.savefig("confusion_matrix_t{}_i{}{}".format(train_number, iteration, name))
+
+def find_accuracy(confusion_matrix):                         
+    #returns a tuple (overall accuracy, average accuracy)
     diagonal = np.diagonal(confusion_matrix)
     correct = np.sum(diagonal)
     total = np.sum(confusion_matrix)
@@ -160,31 +221,45 @@ def find_accuracy(confusion_matrix):                         #returns a tuple (o
     n = 0
     for synapse_type in confusion_matrix:
         row_sum = np.sum(synapse_type)
-        accuracies.append(synapse_type[n]/row_sum)
+        if row_sum > 0:
+            accuracies.append(synapse_type[n]/row_sum)
         n+=1
 
     avg_accuracy = statistics.mean(accuracies)
 
     return (overall_accuracy, avg_accuracy)
 
-def plot_accuracy(db_credentials, predict_path, train_numbers):
+def plot_accuracy(db_credentials, predict_path, 
+                  train_numbers, predict_numbers=None,
+                  train_labels=None):
+
     highest_accuracies = []
     train_avg_accuracies = {}
-    for train_number in train_numbers:
+    if predict_numbers is None:
+        predict_numbers = {t: None for t in train_numbers}
+
+    for i in range(len(train_numbers)):
+        train_number = train_numbers[i]
         setups = os.listdir(predict_path)
         setups = [i for i in setups if i.startswith("setup_t{}".format(train_number))]
-        predict_numbers = [int(i[i.rindex("p")+1:]) for i in setups if i.startswith("setup_t{}".format(train_number))]
-        predict_numbers.sort()
+        predict_numbers_train = predict_numbers[train_number]
+
+        if predict_numbers_train is None:
+            predict_numbers_train = [int(i[i.rindex("p")+1:]) 
+                    for i in setups if i.startswith("setup_t{}".format(train_number))]
+            predict_numbers_train.sort()
+
         overall_accuracies = []
         avg_accuracies = []
         iterations = []
-        for predict_number in predict_numbers:
+        for predict_number in predict_numbers_train:
             synapses, predict_cfg = parse_prediction(db_credentials,
-                                                     predict_path+"setup_t{}_p{}/predict_config.ini".format(train_number, predict_number))
+                                                     predict_path +\
+                        "setup_t{}_p{}/predict_config.ini".format(train_number, predict_number))
             checkpoint = predict_cfg["train_checkpoint"]
             iteration = checkpoint[checkpoint.rindex("_")+1:]
 
-            cm = confusion_matrix(synapses, predict_cfg)
+            cm = synaptic_confusion_matrix(synapses, predict_cfg)
             accuracy = find_accuracy(cm)
             overall_accuracies.append(accuracy[0])
             avg_accuracies.append(accuracy[1])
@@ -193,22 +268,33 @@ def plot_accuracy(db_credentials, predict_path, train_numbers):
         highest_accuracies.append((train_number, max(overall_accuracies),max(avg_accuracies)))
         train_avg_accuracies[train_number] = avg_accuracies
 
-        plt.scatter(iterations, overall_accuracies, label="t{} overall accuracy".format(train_number))
-        plt.plot(iterations, overall_accuracies)
-        plt.legend()
+        label=None
+        if train_labels is not None:
+            label = train_labels[train_number]
+        sn.lineplot(iterations, overall_accuracies, marker='*', label=label)
+ 
+        plt.xlabel("Iteration")
+        plt.ylabel("Accuracy [Total]")
+        print("Overall: t{}".format(train_number))
+        print(iterations)
+        print(overall_accuracies)
 
-    f = open("t{}_highest_accuracies.txt".format(train_numbers),"w+")
-    f.write("(train_number, highest overall accuracy, highest average accuracy)\n")
-    for setup in highest_accuracies:
-        f.write(str(setup)+"\n")
-    f.close()
-    plt.savefig("plot_overall_accuracies_t{}".format(train_numbers))
+    if train_labels is not None:
+        plt.legend()
     plt.show()
 
     for train_number in train_numbers:
         avg_accuracies = train_avg_accuracies[train_number]
-        plt.scatter(iterations, avg_accuracies, label="t{} average accuracy".format(train_number))
-        plt.plot(iterations, avg_accuracies)
+        label=None
+        if train_labels is not None:
+            label = train_labels[train_number]
+        sn.lineplot(iterations, avg_accuracies, marker='*', label=label)
+        plt.xlabel("Iteration")
+        plt.ylabel("Accuracy [Average]")
+        print("Average: t{}".format(train_number))
+        print(iterations)
+        print(avg_accuracies)
+
+    if train_labels is not None:
         plt.legend()
-    plt.savefig("plot_average_accuracies_t{}".format(train_numbers))
     plt.show()
