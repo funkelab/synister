aeda22eb71bd599e305fd4e839574a2b10bf2673
nilsec <nils.eckstein@googlemail.com>
2019-12-12 13:56:02 -0500

    Add write out for synapses [michelle]

--- synister/catmaid_interface.py
+++ synister/catmaid_interface.py
@@ -5,6 +5,7 @@ import numpy as np
 import vispy
 import plotly.io as pio
 import plotly
+import json
 import random
 from synister.synister_db import SynisterDb
 pio.renderers.default = "browser"
@@ -78,7 +79,7 @@ class Catmaid(object):
         data["layout"]["scene"]["zaxis"]["range"] = [-350000, -50000]
         data["layout"]["scene"]["zaxis"]["dtick"] = 50000
         data["layout"]["scene"]["aspectmode"] = "cube"
-        
+
         plotly.offline.plot(data, filename="plot_neurons", image="svg")
 
     def plot_brain_regions(self, test_brain_regions, train_brain_regions, connectors=True):
@@ -100,15 +101,106 @@ class Catmaid(object):
                 train_skids.add(skid)
             else:
                 test_skids.add(skid)
-    
+
         test_skids = list(test_skids)
         train_skids = list(train_skids)
         if len(test_skids) > 25:
             test_skids = random.sample(test_skids, 25)
         if len(train_skids) >25:
             train_skids = random.sample(train_skids, 25)
-        
+
         if test_boolean == True:
             self.plot_neurons(test_skids)
         else:
             self.plot_neurons(train_skids)
+
+    def write_split_to_file(self, db_credentials, db_name, split_name, neurotransmitters, path):
+        db = SynisterDb(db_credentials, db_name)
+
+        f = open(path+"{}_by_nt.txt".format(split_name), "w+")
+        all_test = set()
+        all_train = set()
+        not_in_catmaid = set()
+
+        for nt in neurotransmitters:
+            test_skids = set()
+            train_skids = set()
+            overlap = set()
+            test_brain_regions = set()
+            train_brain_regions = set()
+            test_synapses = set()
+            train_synapses = set()
+
+            synapses = db.get_synapses(neurotransmitters=(nt,))
+
+            for synapse_id, synapse in synapses.items():
+                skid = synapse["skeleton_id"]
+
+                if skid not in all_test and skid not in all_train:                  #Checks if is in Catmaid
+                    try:
+                        pymaid.get_neurons([skid])
+
+                        if synapse["splits"][split_name] == "train":
+                            train_skids.add(skid)
+                            all_train.add(skid)
+                            train_brain_regions.update(synapse["brain_region"])
+                            train_synapses.add(synapse_id)
+                            if skid in test_skids:
+                                overlap.add(skid)
+                        else:
+                            test_skids.add(skid)
+                            all_test.add(skid)
+                            test_brain_regions.update(synapse["brain_region"])
+                            test_synapses.add(synapse_id)
+                            if skid in train_skids:
+                                overlap.add(skid)
+
+
+                    except:
+                        not_in_catmaid.add(skid)
+                        print("Skid not in catmaid {}".format(skid))
+
+
+
+
+
+            test_skids = list(test_skids)
+            train_skids = list(train_skids)
+
+            f.write("Neurotransmitter: {}\n".format(nt))
+            f.write("Test skids: {}\n".format(test_skids))
+            f.write("Train skids: {}\n".format(train_skids))
+            f.write("Overlap: {}\n".format(overlap))
+            if split_name == "brain_region":
+                f.write("Test brain regions: {}\n".format(test_brain_regions))
+                f.write("Train brain regions: {}\n".format(train_brain_regions))
+                # f.write("Test synapses: {}\n".format(test_synapses))
+                # f.write("Train synapses: {}\n".format(train_synapses))
+            f.write("\n\n")
+
+        f.write("All test: {}\n".format(list(all_test)))
+        f.write("All train: {}\n".format(list(all_train)))
+        print(not_in_catmaid)
+
+
+    def write_synapse_locations(self, db_credentials, db_name, split_name, neurotransmitters, path):
+        db = SynisterDb(db_credentials, db_name)
+
+        data = {}
+
+        for nt in neurotransmitters:
+            data[nt] = {"test":{},"train":{}}
+
+            synapses = db.get_synapses(neurotransmitters=(nt,))
+
+            for synapse_id, synapse in synapses.items():
+                skid = synapse["skeleton_id"]
+                synapse_location = tuple((synapse["x"]/10000.0, synapse["z"]/10000.0, synapse["y"]/-10000.0))
+
+                if skid not in data[nt][synapse["splits"][split_name]]:
+                    data[nt][synapse["splits"][split_name]][skid] = []
+
+                data[nt][synapse["splits"][split_name]][skid].append(synapse_location)
+
+        with open(path+"{}_synapse_locations.json".format(split_name), "w") as f:
+            json.dump(data, f)
