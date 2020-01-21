0855b59808c033263b996a2277eef158b1b6f169
nilsec <nils.eckstein@googlemail.com>
2020-01-21 14:52:02 -0500

    Make multi run generation possible

--- prepare_prediction.py
+++ prepare_prediction.py
@@ -1,4 +1,5 @@
 import os
+from copy import deepcopy
 import sys
 from shutil import copyfile, rmtree
 import json
@@ -6,18 +7,60 @@ import configargparse
 import configparser
 from os.path import expanduser
 import click
-from synister.read_config import read_train_config
+from synister.read_config import read_train_config, read_predict_config
 
 
 p = configargparse.ArgParser()
 p.add('-d', '--base_dir', required=True, help='base directory for storing synister experiments')
 p.add('-e', required=True, help='name of the experiment, e.g. fafb, defaults to ``base``')
 p.add('-t', required=True, help='train setup number to use for this prediction')
-p.add('-i', required=True, help='iteration checkpoint number to use for this prediction')
-p.add('-p', required=True, help='predict number/id to use for this prediction')
+p.add('-i', nargs="+",required=True, help='iterations to use for validation')
 p.add('-v', required=False, action='store_true', help='use validation split part')
 p.add('-c', required=False, action='store_true', help='clean up - remove specified predict setup')
 
+def set_up_environments(base_dir,
+                        experiment,
+                        train_number,
+                        iterations,
+                        clean_up,
+                        validation):
+
+    #predict_setup_dir = os.path.join(os.path.join(base_dir, experiment), "03_predict/setup_t{}_p{}".format(train_number, predict_number))
+    predict_setup_dir = os.path.join(os.path.join(base_dir, experiment), "03_predict")
+
+    # Predict runs of the same train run that already exist:
+    existing_predicts = [os.path.join(predict_setup_dir, f) for f in os.listdir(predict_setup_dir) if "setup_t{}".format(train_number) in f]
+    existing_predict_configs = [read_predict_config(os.path.join(f, "predict_config.ini")) for f in existing_predicts]
+    existing_predict_numbers = [cfg["predict_number"] for cfg in existing_predict_configs]
+
+    # Check for overlapping already existing predicts:
+    collisions = {i: None for i in iterations}
+    for cfg in existing_predict_configs:
+        i_cfg = int(cfg["train_checkpoint"].split("_")[-1])
+        val_cfg = cfg["split_part"] == "validation"
+
+        if val_cfg == validation:
+            if i_cfg in iterations:
+                collisions[i_cfg] = cfg["predict_number"]
+
+    predict_number_new = max(existing_predict_numbers) + 1
+    for iteration in iterations:
+        if collisions[iteration] is not None:
+            predict_number = collisions[iteration]
+        else:
+            predict_number = predict_number_new
+        print("Create {} for iteration {}".format("{}/setup_t{}_p{}".format(experiment, train_number, predict_number), iteration))
+
+        set_up_environment(base_dir,
+                           experiment,
+                           train_number,
+                           iteration,
+                           predict_number,
+                           clean_up,
+                           validation)
+
+        if collisions[iteration] is None:
+            predict_number_new += 1
 
 def set_up_environment(base_dir,
                        experiment,
@@ -33,6 +76,9 @@ def set_up_environment(base_dir,
     train_config = os.path.join(train_setup_dir, "train_config.ini")
     train_worker_config = os.path.join(train_setup_dir, "worker_config.ini")
 
+    if not os.path.exists(train_checkpoint):
+        raise ValueError("No checkpoint at {}".format(train_checkpoint))
+
     if clean_up:
         if __name__ == "__main__":
             if click.confirm('Are you sure you want to remove {} and all its contents?'.format(predict_setup_dir), default=False):
@@ -40,38 +86,36 @@ def set_up_environment(base_dir,
             else:
                 print("Abort clean up")
 
-    if not os.path.exists(train_checkpoint):
-        raise ValueError("No checkpoint at {}".format(train_checkpoint))
-
-    if not os.path.exists(predict_setup_dir):
-        os.makedirs(predict_setup_dir)
     else:
-        if __name__ == "__main__":
-            if click.confirm('Predict setup {} exists already, overwrite?'.format(predict_setup_dir), default=False):
-                rmtree(predict_setup_dir)
-                os.makedirs(predict_setup_dir)
-            else:
-                print("Abort.")
-                return
+        if not os.path.exists(predict_setup_dir):
+            os.makedirs(predict_setup_dir)
         else:
-            raise ValueError("Predict setup exists already, choose different predict number or clean up.")
+            if __name__ == "__main__":
+                if click.confirm('Predict setup {} exists already, overwrite?'.format(predict_setup_dir), default=False):
+                    rmtree(predict_setup_dir)
+                    os.makedirs(predict_setup_dir)
+                else:
+                    print("Abort.")
+                    return
+            else:
+                raise ValueError("Predict setup exists already, choose different predict number or clean up.")
 
-    copyfile("synister/predict_pipeline.py", os.path.join(predict_setup_dir, "predict_pipeline.py"))
-    copyfile("synister/predict.py", os.path.join(predict_setup_dir, "predict.py"))
-    copyfile(train_worker_config, os.path.join(predict_setup_dir, "worker_config.ini"))
- 
-    train_config_dict = read_train_config(train_config)
+        copyfile("synister/predict_pipeline.py", os.path.join(predict_setup_dir, "predict_pipeline.py"))
+        copyfile("synister/predict.py", os.path.join(predict_setup_dir, "predict.py"))
+        copyfile(train_worker_config, os.path.join(predict_setup_dir, "worker_config.ini"))
+     
+        train_config_dict = read_train_config(train_config)
 
-    predict_config = create_predict_config(base_dir,
-                                           experiment,
-                                           train_number,
-                                           predict_number,
-                                           train_checkpoint,
-                                           train_config_dict,
-                                           validation)
+        predict_config = create_predict_config(base_dir,
+                                               experiment,
+                                               train_number,
+                                               predict_number,
+                                               train_checkpoint,
+                                               train_config_dict,
+                                               validation)
 
-    with open(os.path.join(predict_setup_dir, "predict_config.ini"), "w+") as f:
-        predict_config.write(f)
+        with open(os.path.join(predict_setup_dir, "predict_config.ini"), "w+") as f:
+            predict_config.write(f)
 
 def create_predict_config(base_dir,
                           experiment,
@@ -124,15 +168,13 @@ if __name__ == "__main__":
     base_dir = options.base_dir
     experiment = options.e
     train_number = int(options.t)
-    train_iteration = int(options.i)
-    predict_number = int(options.p)
+    train_iterations = [int(i) for i in options.i]
     clean_up = bool(options.c)
     validation = bool(options.v)
 
-    set_up_environment(base_dir,
+    set_up_environments(base_dir,
                        experiment,
                        train_number,
-                       train_iteration,
-                       predict_number,
+                       train_iterations,
                        clean_up,
                        validation)
